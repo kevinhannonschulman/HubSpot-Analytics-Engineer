26368c0587e379471718d54eacfda99517ccf854
Kevin Hannon Schulman <kevinhannon95@gmail.com>
2026-04-08 21:46:00 +0000

    revised stg_listings and yml

--- models/staging/stg_listings.sql
+++ models/staging/stg_listings.sql
@@ -2,39 +2,21 @@ with source as (
     select * from {{source('dbt_models', 'Listings')}}
 )
 
+--narrowing data down to the columns that will answer questions 1-3 and excluding extraneous information--
+
 , reformat as (
     select id as listing_id
-    , name as property_name
-    , host_id
-    , date(host_since) as host_join_date
     , host_location
-    , replace(host_verifications, '"', '') as host_verifications --removed quotes from json--
     , neighborhood
-    , property_type
-    , room_type
-    , accommodates as max_people
-    , bathrooms_text as num_baths
-    , bedrooms
-    , beds
-    , replace((amenities), '"', '') as amenities --removed quotes from json--
+    , replace((amenities), '"', '') as amenities --removed quotes--
     from source
 )
 
 , final as (
     select listing_id
-    , property_name
-    , host_id
-    , host_join_date
     , host_location
-    , trim(host_verifications, '[]') as host_verifications --removed brackets from json--
     , neighborhood
-    , property_type
-    , room_type
-    , max_people
-    , num_baths
-    , bedrooms
-    , beds
-    , trim((amenities), '[]') as amenities --removed brackets from json--
+    , trim((amenities), '[]') as amenities --removed brackets. using replace with both brackets and quotes didn't remove both so needed to use two CTEs--
     from reformat
 )
 
